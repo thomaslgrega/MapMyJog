0bc3037c9474841ef0a01cfdfeda9e0abec54fb6
tgrega <zeroyontlg@yahoo.com>
2020-08-15 22:58:09 -0700

    Implement User model, Users controller, sessions controller (partial user Auth)

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login!(@user)
+      render api_user_url(@user)
+      # render "/api/users/#{@user.id}"
+    else
+      render json: ['Invalid username or password'], status: 422
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout
+      render json: {}
+    else
+      render json: ['No current user'], status: 404
+    end
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  helper_method :logged_in?, :current_user
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout
+    @current_user = current_user
+    session[:session_token] = nil
+    @current_user.reset_session_token!
+    @current_user = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
