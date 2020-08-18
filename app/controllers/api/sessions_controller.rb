17bbc598ad779cb3dde5fd50e079e657de7fecfc
tgrega <zeroyontlg@yahoo.com>
2020-08-17 22:29:19 -0700

    Add AuthRoute for login and signup

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,7 @@
 class Api::SessionsController < ApplicationController
   def create
     @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-    if logged_in?
-      render json: ['Already logged in'], status: 422
-    elsif @user
+    if @user
       login!(@user)
       render "/api/users/show"
     else
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,8 @@ import NavBarContainer from './nav_bar_container';
 import { Route } from 'react-router-dom';
 import LoginFormContainer from './login_form_container';
 import SignupFormContainer from './signup_form_container';
+import { AuthRoute } from '../util/route_util';
+
 
 const App = () => {
   return (
@@ -12,8 +14,8 @@ const App = () => {
         <NavBarContainer />
       </header>
 
-      <Route path='/login' component={LoginFormContainer} />
-      <Route path='/signup' component={SignupFormContainer} />
+      <AuthRoute path='/login' component={LoginFormContainer} />
+      <AuthRoute path='/signup' component={SignupFormContainer} />
     </div>
   )
 };
