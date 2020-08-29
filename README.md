2283c6e292220ceb585445877458899ac1ee6946
tgrega <zeroyontlg@yahoo.com>
2020-08-28 20:15:39 -0700

    Add README and photos for it

--- README.md
+++ README.md
@@ -1,24 +1,36 @@
-# README
+<p align="left"><img width="" height="" src="/Users/thomasgrega/Desktop/mapmyjog/app/assets/images/github_photos/mapmyjog_logo.png"></p>
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+MapMyJog is a fitness app that takes advantage of the google maps and directions api to plot waypoints on a map and create a route. It also allows users to add descriptions and types of activities as well as see the distance of the route. Users can then save those routes, allowing them edit them, or delete them at a later time. To add a social aspect, you can access a friends list and go to their profile and see their routes.
 
-Things you may want to cover:
+## Live Link: https://tg-mapmyjog.herokuapp.com/
 
-* Ruby version
+## Technologies Used
+### Frontend
+* React
+* Redux
 
-* System dependencies
+### Backend
+* Ruby on Rails
 
-* Configuration
+### 
+* Google Maps API
 
-* Database creation
+## User Auth
 
-* Database initialization
+<p align="left"><img width="" height="" src="/Users/thomasgrega/Desktop/mapmyjog/app/assets/images/github_photos/user_auth.png"></p>
 
-* How to run the test suite
+Users can sign up with a new account or log in with an account that they've created. A demo login user is also available to allow full testing of the app's features.
 
-* Services (job queues, cache servers, search engines, etc.)
+<p align="left"><img width="" height="" src="/Users/thomasgrega/Desktop/mapmyjog/app/assets/images/github_photos/maps.png"></p>
 
-* Deployment instructions
+Clicking the Create Route button on the dashboard will take users to a google map. By adding a click event listener on the map, users can add waypoints. Using these waypoints and the Google Directions API, the map will create a route from waypoint to waypoint in the order that they're plotted. 
 
-* ...
+## snippet here!
+
+Storing these waypoints in an array and some simple manipulation, we can do some cool things like undo a marker, reset markers, return the last marker to the first waypoint to make a full circle, and reverse a route with a click of a button. 
+
+## Future Direction
+
+- Add an activities feature where users can add and keep track of a specific workout that they've done or plan to do.
+- Add a search feature for finding other users and also searching for routes by other people.
+- Add a commenting feature to comment on other people's routes and workouts if they're made public
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,21 @@
 
 User.delete_all
 Route.delete_all
+Friend.delete_all
 
 ############### USERS #################
+
 demo = { email: 'DemoUser@demo.com', password: 'password', first_name: 'Demo', last_name: 'User', date_of_birth: '01-01-1920', gender: 'male' }
-User.create!(demo)
+demo = User.create!(demo)
 
 demo_friend = { email: 'DemoFriend@demo.com', password: 'password', first_name: 'Friends', last_name: 'Happy', date_of_birth: '11-05-2000', gender: 'female' }
-User.create!(demo_friend)
+demo_friend = User.create!(demo_friend)
 
 more_demo_friends = { email: 'MoreFriends@demo.com', password: 'password', first_name: 'More', last_name: 'Friends', date_of_birth: '20-02-1970', gender: 'male' }
-User.create!(more_demo_friends)
+more_demo_friends = User.create!(more_demo_friends)
 
 one_more = { email: 'friendly@demo.com', password: 'password', first_name: 'something', last_name: 'last', date_of_birth: '20-07-1990', gender: 'female' }
-User.create!(one_more)
+one_more = User.create!(one_more)
 
 ############### ROUTES #################
 
@@ -30,10 +32,10 @@ Route.create!(ggp_route)
 test_route1 = { name: 'Run through city', creator_id: demo.id, distance: '4.7 MI', description: 'Random test Route', waypoints: "[{\"lat\":37.79466608207805,\"lng\":-122.43397003173827},{\"lat\":37.760746076774204,\"lng\":-122.44993453979491},{\"lat\":37.747037980949024,\"lng\":-122.42710357666014}]", activity: "Bike Ride"}
 Route.create!(test_route1)
 
-test_route2 = { name: 'Test Route', creator_id: demo_friend[id], distance: '5.4 MI', description: 'Doesnt\'matter', waypoints: "[{\"lat\":37.79175381254094,\"lng\":-122.43791824340819},{\"lat\":37.776151928302426,\"lng\":-122.45662933349608},{\"lat\":37.766653516758375,\"lng\":-122.42109542846678}]", activity: "Walk"}
+test_route2 = { name: 'Test Route', creator_id: demo_friend.id, distance: '5.4 MI', description: 'Doesnt\'matter', waypoints: "[{\"lat\":37.79175381254094,\"lng\":-122.43791824340819},{\"lat\":37.776151928302426,\"lng\":-122.45662933349608},{\"lat\":37.766653516758375,\"lng\":-122.42109542846678}]", activity: "Walk"}
 Route.create!(test_route2)
 
-test_route3 = { name: 'Cool Walk', creator_id: demo_friend[id], distance: '3.0 MI', description: 'Walk along the beach', waypoints: "[{\"lat\":37.7703919717746,\"lng\":-122.51086519492901},{\"lat\":37.72772092071647,\"lng\":-122.50673750065698}]", activity: "Walk"}
+test_route3 = { name: 'Cool Walk', creator_id: demo_friend.id, distance: '3.0 MI', description: 'Walk along the beach', waypoints: "[{\"lat\":37.7703919717746,\"lng\":-122.51086519492901},{\"lat\":37.72772092071647,\"lng\":-122.50673750065698}]", activity: "Walk"}
 Route.create!(test_route3)
 
 lake_merced = { name: 'lake merced jog', creator_id: demo.id, distance: '2.2 MI', description: 'Great short run at Lake Merced', waypoints: '[{"lat":37.708428,"lng":-122.485855},{"lat":37.729603,"lng":-122.493640}]', activity: 'Run' }
@@ -47,8 +49,6 @@ friendship2 = { user_id: demo.id, friend_id: more_demo_friends.id }
 Friend.create!(friendship2)
 friendship3 = { user_id: demo.id, friend_id: one_more.id }
 Friend.create!(friendship3)
-friendship4 = { user_id: demo.id, friend_id: demo_friend.id }
-Friend.create!(friendship4)
 friendship5 = { user_id: demo_friend.id, friend_id: demo.id }
 Friend.create!(friendship5)
 friendship6 = { user_id: demo_friend.id, friend_id: one_more.id }
