47c6f50f307dc8a99cd8ea1f3e2bf9f4d50f4b93
tgrega <zeroyontlg@yahoo.com>
2020-09-22 19:45:01 -0700

    Add footer and styling

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -11,32 +11,37 @@ const Footer = () => {
           <span className='footer-verb'>RUN</span>
           <span className='footer-verb'>WALK</span>
           <span className='footer-verb'>FITNESS</span>
-
         </div>
       </div>
-      <div className='second-band'>
-        <div className='second-band-left'>
-          <div className='second-band-left-div'>
-            <span className='left-div-title'>SOCIAL</span>
-            <a href="https://github.com/thomaslgrega" target="_blank" className="fab fa-github-square footer-personal-link"></a>
-            <a href="https://www.linkedin.com/in/thomas-grega-4931711b6/" target="_blank" className="fab fa-linkedin footer-personal-link"></a>
-            <a href="https://angel.co/u/thomas-grega" target="_blank" className="fab fa-angellist footer-personal-link"></a>
+      <div className='second-band-background'>
+        <div className='second-band'>
+          <div className='second-band-left'>
+            <div className='second-band-left-div'>
+              <span className='left-div-title'>SOCIAL</span>
+              <a href="https://www.linkedin.com/in/thomas-grega-4931711b6/" target="_blank" className="footer-personal-link">Connect on LinkedIn</a>
+              <a href="https://angel.co/u/thomas-grega" target="_blank" className="footer-personal-link">Connect on AngelList</a>
+              <a href="https://github.com/thomaslgrega/MapMyJog" target="_blank" className="footer-personal-link">GitHub Repo</a>
+            </div>
+            <div className='second-band-left-div'>
+              <span className='left-div-title'>QUICK LINKS</span>
+              <Link to='/login' className='footer-auth-links'>Login</Link>
+              <Link to='/signup' className='footer-auth-links'>Signup</Link>
+            </div>
           </div>
-          <div className='second-band-left-div'>
-            <span className='left-div-title'>HELP</span>
-
+          <div className='second-band-right'>
+            <span className='second-band-right-title'>PERSONAL LINKS</span>
+            <div className='second-band-right-links'>
+              <a href="https://github.com/thomaslgrega" target="_blank" className="fab fa-github-square right-band-link"></a>
+              <a href="https://www.linkedin.com/in/thomas-grega-4931711b6/" target="_blank" className="fab fa-linkedin right-band-link"></a>
+              <a href="https://angel.co/u/thomas-grega" target="_blank" className="fab fa-angellist right-band-link-angellist"></a>
+            </div>
           </div>
-          <div className='second-band-left-div'>
-            <span className='left-div-title'>ABOUT ME</span>
-
-          </div>
-        </div>
-        <div className='second-band-right'>
-
         </div>
       </div>
       <div className='third-band'>
-
+        <span>Created by Thomas Grega</span>
+        <span>|</span>
+        <span>Demonstration purposes only</span>
       </div>
     </footer>
   )
